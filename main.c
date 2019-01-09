cdffe70176bf9dd00cf10dc2f4d3e79c75977f1e
Axel GERVEAU <axelgerv@e3r1p12.42.fr>
2019-01-09 15:35:48 +0100

    modification de read_fd

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: axelgerv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/06 15:42:19 by axelgerv          #+#    #+#             */
-/*   Updated: 2019/01/09 14:15:23 by axelgerv         ###   ########.fr       */
+/*   Updated: 2019/01/09 15:34:51 by axelgerv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int		main(int argc, char **argv)
 {
 	int		fd;
-	char	*storage;
+	char	*storage[27];
 	int		nbr_blocks;
 	t_tetri	**element;
 
@@ -25,16 +25,14 @@ int		main(int argc, char **argv)
 	else if (argc == 2)
 	{
 		fd = open(argv[1], O_RDONLY);
-		storage = NULL;
-		nbr_blocks = read_fd(fd, &storage);
+		nbr_blocks = read_fd(fd, storage);
 		if (nbr_blocks == -1)
 			return (0);
 		close(fd);
 	}
 	if (!(element = (t_tetri **)malloc(sizeof(t_tetri *) * (nbr_blocks + 1))))
 		return (0);
-	ft_putstr(storage);
-	ft_putchar('\n');
-	ft_putnbr(nbr_blocks);
+	//ft_putchar('\n');
+	//ft_putstr(storage[0]);
 	return (0);
 }
--- read_fd.c
+++ read_fd.c
@@ -6,7 +6,7 @@
 /*   By: axelgerv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/09 14:14:04 by axelgerv          #+#    #+#             */
-/*   Updated: 2019/01/09 14:14:21 by axelgerv         ###   ########.fr       */
+/*   Updated: 2019/01/09 15:34:26 by axelgerv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,18 @@ int		read_fd(int fd, char **storage)
 
 	nbr_blocks = 0;
 	buf = ft_strnew(BUFF_SIZE);
-	*storage = ft_strnew(BUFF_SIZE);
 	while ((rd = read(fd, buf, BUFF_SIZE)) > 0)
 	{
 		if (check_error(buf) == -1)
 		{
+			ft_strdel(&buf);
 			ft_putstr("error\n");
 			return (-1);
 		}
+		storage[nbr_blocks] = ft_strdup(buf);
 		nbr_blocks++;
-		*storage = ft_strjoinf(storage, &buf, 1);
 		ft_strclr(buf);
 	}
+	ft_strdel(&buf);
 	return (nbr_blocks);
 }
