81fc614734c1f87c3cd03a56b2e88af977c3f789
Axel GERVEAU <axelgerv@e3r1p10.42.fr>
2019-01-10 16:30:19 +0100

    add reducing in storage.c

--- storage.c
+++ storage.c
@@ -6,11 +6,71 @@
 /*   By: axelgerv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/09 14:16:39 by axelgerv          #+#    #+#             */
-/*   Updated: 2019/01/10 13:15:49 by axelgerv         ###   ########.fr       */
+/*   Updated: 2019/01/10 16:29:08 by axelgerv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
+
+int	  check_x(char *buf, int mod)
+{
+	int i;
+	int x;
+
+	mod == 1 ? (i = 0) : (i = 20);
+	mod == 1 ? (x = 5) : (x = 0);
+	if (mod == 1)
+	{
+		while (buf[i])
+		{
+			if (buf[i] == '#' && i % 5 < x)
+				x = i % 5;
+			i++;
+		}
+	}
+	if (mod == -1)
+	{
+		while (i >= 0)
+		{
+			if (buf[i] == '#' && i % 5 > x)
+				x = i % 5;
+			i--;
+		}
+	}
+	return (x);
+}
+
+int		check_y(char *buf, int mod)
+{
+	int i;
+	int y;
+
+	mod == 1 ? (i = 0) : (i = 19);
+	mod == 1 ? (y = 0) : (y = 4);
+	if (mod == 1)
+	{
+		while (buf[i])
+		{
+			if (buf[i] == '#')
+				return (y);
+			if (buf[i] == '\n')
+				y++;
+			i++;
+		}
+	}
+	if (mod == -1)
+	{
+		while (i >= 0)
+		{
+			if (buf[i] == '#')
+				return (y);
+			if (buf[i] == '\n')
+				y--;
+			i--;
+		}
+	}
+	return (y);
+}
 
 t_tetri	*new_element(char **block, int order)
 {
